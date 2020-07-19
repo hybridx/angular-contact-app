40827ed457bf1f18cca59ad767709b66117070e7
hybridx <hybridx18@gmail.com>
2020-07-20 01:16:22 +0530

    Added the basic design structure for the contact app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-contact-app';
+  @ViewChild('modalToggle') modal: ElementRef;
+
+  contacts = [
+    {
+      name: 'Deepesh Nair',
+      email: 'denair@gmail.com',
+      phone: 9988776655,
+      address: 'JDoe 1120',
+      birthday: moment().format('YYYY-MM-DD'),
+    },
+    {
+      name: 'Jayesh Nair',
+      email: 'jnair@gmail.com',
+      phone: 9988776655,
+      address: 'JDoe 1120',
+      birthday: moment().format('YYYY-MM-DD'),
+    },
+    {
+      name: 'TVP Nair',
+      email: 'tvp@gmail.com',
+      phone: 9988776655,
+      address: 'JDoe 1120',
+      birthday: moment().format('YYYY-MM-DD'),
+    },
+  ];
+  modalInfo: UserInfo;
+
+  openModal(contact) {
+    this.modal.nativeElement.click();
+    this.modalInfo = contact;
+  }
 }
---
+++ src/app/type.d.ts
@@ -0,0 +1,8 @@
+type UserInfo = {
+    id: string;
+    name: string,
+    email: string,
+    phone: number,
+    address: string,
+    birthday: Date,
+};
