73e89469a67eff800b194a835f777c4224febace
hybridx <hybridx18@gmail.com>
2020-07-19 19:48:13 +0000

    Deploying to master from  @ 40827ed457bf1f18cca59ad767709b66117070e7 🚀

--- styles-es5.js
+++ styles-es5.js
@@ -17,13 +17,13 @@
 
       exports = ___CSS_LOADER_API_IMPORT___(true); // Module
 
-      exports.push([module.i, "/* You can add global styles to this file, and also import other style files */", "", {
+      exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\nbody {\n  background: #e1e6ed;\n}", "", {
         "version": 3,
         "sources": ["styles.scss"],
         "names": [],
-        "mappings": "AAAA,8EAAA",
+        "mappings": "AAAA,8EAAA;AAEA;EACI,mBAAA;AAAJ",
         "file": "styles.scss",
-        "sourcesContent": ["/* You can add global styles to this file, and also import other style files */\n"]
+        "sourcesContent": ["/* You can add global styles to this file, and also import other style files */\n\nbody {\n    background: #e1e6ed;\n}\n"]
       }]); // Exports
 
       module.exports = exports;
